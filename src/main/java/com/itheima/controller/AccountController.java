aae912663c247ec8062a16541f67442339e815e1
owen-zhou20 <78416546+owen-zhou20@users.noreply.github.com>
2022-04-27 12:01:45 +1000

    test1

---
+++ src/main/java/com/itheima/controller/AccountController.java
@@ -0,0 +1,19 @@
+package com.itheima.controller;
+
+import com.itheima.domain.Account;
+import org.springframework.web.servlet.ModelAndView;
+
+public class AccountController {
+
+    //save
+    public String save(Account account){
+        return null;
+    }
+
+    //select
+    public ModelAndView findAll(){
+        return null;
+    }
+
+
+}
---
+++ src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -0,0 +1,20 @@
+package com.itheima.service.impl;
+
+import com.itheima.domain.Account;
+import com.itheima.service.AccountService;
+
+import java.util.List;
+
+public class AccountServiceImpl implements AccountService {
+
+
+    @Override
+    public void save(Account account) {
+
+    }
+
+    @Override
+    public List<Account> findAll() {
+        return null;
+    }
+}
